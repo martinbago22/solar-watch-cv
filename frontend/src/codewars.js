64fed5936ee8818d65af74fdf1f0bd86e4475066
martinbago22 <bageszka17@gmail.com>
2024-05-21 10:42:41 +0200

    add gitignore

--- frontend/src/codewars.js
+++
@@ -1,9 +0,0 @@
-function getMiddle(s) {
-    if (s.length % 2 === 0) {
-      return s.slice(s.length / 2 - 1, s.length / 2 + 1)
-    } else {
-      return s.slice(s.length / 2, s.length / 2 + 1);
-    }
-  }
-
-console.log(getMiddle("helqo"));
