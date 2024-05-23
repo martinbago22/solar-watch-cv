5cd8a3f539b4e9f0b967b0384bf44165f5d18eaf
martinbago22 <bageszka17@gmail.com>
2024-05-23 13:36:20 +0200

    create Test component for purposes of testing and implement basic user registration method

---
+++ frontend/components/Test.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        {{ message }}
+    </div>
+</template>
+  
+<script setup>
+    import { ref } from 'vue';
+    const message = ref("I am alive");
+    async function registerUser() {
+        const user = {
+            username: "abc123",
+            password: "pw1"
+        };
+        try {
+            const response = await fetch("http://localhost:8080/api/user/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(user)
+            });
+            if (response.ok) {
+                console.log("ok")
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    }
+    registerUser();
+</script>
