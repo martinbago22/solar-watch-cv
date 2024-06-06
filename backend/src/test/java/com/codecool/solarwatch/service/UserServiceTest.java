3a38344a034a0f2378571ed9d30a22fbfaec5860
martinbago22 <bageszka17@gmail.com>
2024-06-06 13:31:31 +0200

    create UserServiceTest class.

---
+++ backend/src/test/java/com/codecool/solarwatch/service/UserServiceTest.java
@@ -0,0 +1,24 @@
+package com.codecool.solarwatch.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void addRoleFor() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void grantAdminPrivilegesFor() {
+    }
+}
