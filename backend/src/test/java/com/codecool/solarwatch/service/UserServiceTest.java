729219ea08d6fdfee590891b2091ee6a1860c8e2
martinbago22 <bageszka17@gmail.com>
2024-06-11 16:50:24 +0200

    create UserServiceTest and initialize mocked dependencies.

--- backend/src/test/java/com/codecool/solarwatch/service/UserServiceTest.java
+++ backend/src/test/java/com/codecool/solarwatch/service/UserServiceTest.java
@@ -1,10 +1,21 @@
 package com.codecool.solarwatch.service;
 
+import com.codecool.solarwatch.repository.RoleRepository;
+import com.codecool.solarwatch.repository.UserRepository;
+import com.codecool.solarwatch.security.jwt.JwtUtils;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.mockito.Mockito;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 class UserServiceTest {
+    UserRepository userRepository = Mockito.mock(UserRepository.class);
+    RoleRepository repository = Mockito.mock(RoleRepository.class);
+    AuthenticationManager authenticationManager = Mockito.mock(AuthenticationManager.class);
+    PasswordEncoder passwordEncoder = Mockito.mock(PasswordEncoder.class);
+    JwtUtils jwtUtils = Mockito.mock(JwtUtils.class);
+    UserService userService = new UserService(userRepository,
+            repository, authenticationManager, passwordEncoder, jwtUtils);
 
     @Test
     void addRoleFor() {
