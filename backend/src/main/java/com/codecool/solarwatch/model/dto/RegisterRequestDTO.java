f442083b109d2d04b90f6b1ba82f82b95622321e
martinbago22 <bageszka17@gmail.com>
2024-06-20 11:27:52 +0200

    add new constraints to password field

--- backend/src/main/java/com/codecool/solarwatch/model/dto/RegisterRequestDTO.java
+++ backend/src/main/java/com/codecool/solarwatch/model/dto/RegisterRequestDTO.java
@@ -4,12 +4,13 @@ package com.codecool.solarwatch.model.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-//TODO DTO-t tesztelni.
 
 public record RegisterRequestDTO(
         @NotBlank(message = "Username cannot be empty")
         @Pattern(regexp = "^[a-zA-Z0-9.]*$", message = "Username cannot contain any special characters")
         @Size(min = 4, max = 10, message = "Username must be between 4 and 10 characters long")
         String username,
+        @NotBlank(message = "Password must be minimum 4, maximum 10 characters long")
+        @Size(message = "Password must be minimum 4, maximum 10 characters long")
         String password) {
 }
