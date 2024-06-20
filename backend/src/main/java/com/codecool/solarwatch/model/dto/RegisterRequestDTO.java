bb3938957b21d8dec00c74559e90315916f59b46
martinbago22 <bageszka17@gmail.com>
2024-06-20 11:17:28 +0200

    organize test cases into nested classes for better readability

--- backend/src/main/java/com/codecool/solarwatch/model/dto/RegisterRequestDTO.java
+++ backend/src/main/java/com/codecool/solarwatch/model/dto/RegisterRequestDTO.java
@@ -1,11 +1,13 @@
 package com.codecool.solarwatch.model.dto;
 
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 //TODO DTO-t tesztelni.
 
 public record RegisterRequestDTO(
+        @NotBlank(message = "Username cannot be empty space")
         @Pattern(regexp = "^[a-zA-Z0-9.]*$", message = "Username cannot contain any special characters")
         @Size(min = 4, max = 10, message = "Username must be between 4 and 10 characters long")
         String username,
