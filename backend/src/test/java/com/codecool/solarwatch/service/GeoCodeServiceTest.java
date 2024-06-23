a13c7b94dfbc67503fcb9bacaf49c103e12a5cde
martinbago22 <bageszka17@gmail.com>
2024-06-23 20:52:45 +0200

    mess with webclient retrieval in coordinate fetcher, currently broke the code, review tomorrow.

--- backend/src/test/java/com/codecool/solarwatch/service/GeoCodeServiceTest.java
+++ backend/src/test/java/com/codecool/solarwatch/service/GeoCodeServiceTest.java
@@ -9,19 +9,22 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @DisplayName("GeocodeService Unit test")
+@ExtendWith(MockitoExtension.class)
 class GeoCodeServiceTest {
     @Mock
     CoordinateFetcher coordinateFetcher;
     @InjectMocks
     GeoCodeService geoCodeService;
-    @ExtendWith(MockitoExtension.class)
+
     @Test
     void WhenGetCoordinatesFromCity_ThenCoordinatesGetReturned() {
         Coordinates expected = new Coordinates(0, 1, "Hungary", "Bács-Kiskun");
@@ -35,8 +38,8 @@ class GeoCodeServiceTest {
 
     @Test
     void WhenCityIsNull_ThenInvalidCityExceptionIsThrown() {
-        when(coordinateFetcher.getCoordinatesForCity(anyString()))
-                .thenReturn(null);
+        when(coordinateFetcher.getCoordinatesForCity(null))
+                .thenReturn(any(Coordinates.class));
 
         assertThrows(InvalidCityException.class, () -> this.geoCodeService
                 .getCoordinatesFromCity(null));
@@ -44,7 +47,11 @@ class GeoCodeServiceTest {
 
     @Test
     void getCoordinatesFromCityThrowsInvalidCityExceptionForInvalidCityName() {
+        String invalidCityName = "123123123";
+        /*when(coordinateFetcher.getCoordinatesForCity(invalidCityName))
+                .thenReturn(Mono.just(null));*/
+
         assertThrows(InvalidCityException.class, () -> this.geoCodeService
-                .getCoordinatesFromCity("123213"));
+                .getCoordinatesFromCity(invalidCityName));
     }
 }
