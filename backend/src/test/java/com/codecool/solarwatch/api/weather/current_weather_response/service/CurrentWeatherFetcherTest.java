5070627521f65629bac6a490d84b7eb709da78cf
martinbago22 <bageszka17@gmail.com>
2024-06-25 11:05:46 +0200

    initalize CurrentWeatherFetcherTest

---
+++ backend/src/test/java/com/codecool/solarwatch/api/weather/current_weather_response/service/CurrentWeatherFetcherTest.java
@@ -0,0 +1,22 @@
+package com.codecool.solarwatch.api.weather.current_weather_response.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+@DisplayName("CurrentWeatherFetcher Unit test")
+class CurrentWeatherFetcherTest {
+    @Nested
+    @DisplayName("Test cases for successful SunriseSunsetInfo fetching")
+    class WhenFetchSunriseSunsetInfoIsSuccessful {
+        @Test
+        @DisplayName("asd")
+        void WhenGivenValidCityAndDate_ThenFetchingIsSuccessful() {
+
+        }
+    }
+}
