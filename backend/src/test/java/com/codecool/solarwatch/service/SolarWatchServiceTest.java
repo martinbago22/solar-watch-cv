c8f068e6f29cdef917147ff779f3bbb15c9b4b46
martinbago22 <bageszka17@gmail.com>
2024-07-10 13:32:37 +0200

    provide test for getting sunrise sunset info with invalid date parameter

--- backend/src/test/java/com/codecool/solarwatch/service/SolarWatchServiceTest.java
+++ backend/src/test/java/com/codecool/solarwatch/service/SolarWatchServiceTest.java
@@ -2,26 +2,22 @@ package com.codecool.solarwatch.service;
 
 import com.codecool.solarwatch.api.geocoding.service.GeoCodeAPIFetcher;
 import com.codecool.solarwatch.exception.InvalidDateException;
-import com.codecool.solarwatch.exception.NotSupportedCityException;
-import com.codecool.solarwatch.model.entity.City;
-import com.codecool.solarwatch.model.entity.SunriseSunsetInfo;
 import com.codecool.solarwatch.repository.CityRepository;
 import com.codecool.solarwatch.repository.SunriseSunsetRepository;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.*;
-@DisplayName("OpenWeatherService Unit test")
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@DisplayName("SolarWatchService Unit test")
 @ExtendWith(MockitoExtension.class)
 class SolarWatchServiceTest {
     @Mock
@@ -33,10 +29,22 @@ class SolarWatchServiceTest {
     @InjectMocks
     SolarWatchService solarWatchService;
 
-    @Test
-    @DisplayName("getSunriseSunsetInfo throws invalid date exception when provided invalid date")
-    void WhenProvidedInvalidDateParameter_ThenGetSunriseSunsetInfoThrowsInvalidDateException() {
-        assertThrows(InvalidDateException.class,
-                () -> solarWatchService.getSunriseSunsetInfo("asd", "asd"));
+    @Nested
+    @DisplayName("getSunriseSunsetInfo method tests")
+    class GetSunriseSunsetInfoTests {
+        @Test
+        @DisplayName("getSunriseSunsetInfo throws invalid date exception when provided an invalid date")
+        void WhenProvidedInvalidDateParameter_ThenGetSunriseSunsetInfoThrowsInvalidDateException() {
+            String invalidDate = "asd";
+            String cityName = "asd";
+            String expectedErrorMessage = "Date is in invalid format. Please provide it with YYYY-MM-DD";
+
+            InvalidDateException exception = assertThrows(InvalidDateException.class,
+                    () -> solarWatchService.getSunriseSunsetInfo(cityName, invalidDate));
+
+
+            verify(cityRepository, times(0)).findByName(cityName);
+            assertEquals(expectedErrorMessage, exception.getMessage());
+        }
     }
 }
