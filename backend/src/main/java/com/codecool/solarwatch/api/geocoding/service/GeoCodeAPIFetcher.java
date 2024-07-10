5adde1b910cc8630e77ccc264bab01bd34dfc00d
martinbago22 <bageszka17@gmail.com>
2024-07-10 14:38:01 +0200

    fix handling the case when user provides invalid cityName where geocoding API returns an empty response array with 200 status even though the search is wrong.

--- backend/src/main/java/com/codecool/solarwatch/api/geocoding/service/GeoCodeAPIFetcher.java
+++ backend/src/main/java/com/codecool/solarwatch/api/geocoding/service/GeoCodeAPIFetcher.java
@@ -1,6 +1,7 @@
 package com.codecool.solarwatch.api.geocoding.service;
 
 import com.codecool.solarwatch.api.geocoding.model.Coordinates;
+import com.codecool.solarwatch.exception.CityNotFoundException;
 import jakarta.validation.constraints.NotBlank;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,6 +28,7 @@ public class GeoCodeAPIFetcher {
     public Coordinates getCoordinatesFromCityName(@NotBlank(message = "City parameter cannot be empty or null") String cityName) {
         String url = String.format("http://api.openweathermap.org/geo/1.0/direct?q=%s&limit=1&appid=%s",
                 cityName, API_KEY);
+
         return webClient.get()
                 .uri(url)
                 .accept(MediaType.APPLICATION_JSON)
@@ -38,7 +40,7 @@ public class GeoCodeAPIFetcher {
                         return Mono.just(coordinatesList.getFirst());
                     } else {
                         LOGGER.error("No city found under name: [{}]", cityName);
-                        return Mono.empty();
+                        return Mono.error(new CityNotFoundException(cityName));
                     }
                 })
                 .block();
